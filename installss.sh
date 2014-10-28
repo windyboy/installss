d2cb9f024b472f5575ee5ea50b4c22c62948651e
windybo <windyboy@gmail.com>
2014-10-28 15:22:50 +0800

    install puppet and add comment

--- installss.sh
+++ installss.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+#check root user
 if [ $USER != "root" ]
 then
     echo "please install with root user !"
     exit
 fi
 
+#get config file
 wget http://windy.me/sss/shadowsocks.pp
 wget http://windy.me/sss/shadowsocks.json
 wget http://windy.me/sss/shadowsocks.conf
+#install puppet repo
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+dpkg -i puppetlabs-release-precise.deb
 
 apt update
 apt upgrade -y
+apt install puppet
+
