407f28abc705ddf812a663e2d15fa1ce8f1eb870
windyboy <windyboy@gmail.com>
2014-10-29 15:31:31 +0800

    fix  bugs

--- shadowsocks.pp
+++ shadowsocks.pp
@@ -7,24 +7,17 @@ package {"shadowsocks":
 	ensure => "installed",
 	provider => "pip"
 }
-service { "supervisor": 
-    ensure => "running",
-    enable  => "true"
-}
 file { "/etc/shadowsocks.json":
 	ensure => "present",
 	source => "$dir/shadowsocks.json",
-    notify => Service ["supervisor"]
 } 
 file { "/etc/supervisor/conf.d/shadowsocks.conf":
 	ensure => "present",
 	source => "$dir/shadowsocks.conf",
-    require => Package["shadowsocks"]
 }
 
 exec {"supervisorctl reload":
 	command => "/usr/bin/supervisorctl reload",
 	logoutput => "true",
-    require => File["/etc/supervisor/conf.d/shadowsocks.conf"],
-    notify => Service ["supervisor"]
+    require => File["/etc/supervisor/conf.d/shadowsocks.conf"]
 }
