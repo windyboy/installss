ca1204cb16fc2708b26273ce788f7a7783b6aa1d
windybo <windyboy@gmail.com>
2014-10-29 11:16:12 +0800

    password and port validation

--- installss.sh
+++ installss.sh
@@ -24,9 +24,19 @@ wget http://windy.me/sss/shadowsocks.pp
 sed -i "s@home@$dir@" shadowsocks.pp 
 wget http://windy.me/sss/shadowsocks.json
 read -p "Please input shadowsocks server password:" pw
+if [ pw = "" ]
+then 
+    echo "password can not be null, reset to default 'windyboy'"
+    pw="windyboy"
+fi
 sed -i "s@windyboy@$pw@" shadowsocks.json
 port=8765
 read -p "Please input server port:" port
+re='^[0-9]+$'
+if ![[$port =~ $re ]]; then
+    echo "server port must be number, reset to default '8765'"
+    port=8765
+fi
 sed -i "s@8765@$port@" shadowsocks.json
 
 #wget http://windy.me/sss/shadowsocks.conf
