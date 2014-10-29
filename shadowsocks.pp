5e98a104d0b3c98b95a18a3212215bcb3cd5e45c
windyboy <windyboy@gmail.com>
2014-10-29 14:39:21 +0800

    fix puppet shadowsocks bugs

--- shadowsocks.pp
+++ shadowsocks.pp
@@ -7,24 +7,25 @@ package {"shadowsocks":
 	ensure => "installed",
 	provider => "pip"
 }
+service { "supervisor": 
+    ensure => "running",
+    require Package["shadowsocks"]
+}
 file { "/etc/shadowsocks.json":
 	ensure => "present",
-	source => "$dir/shadowsocks.json"
+	source => "$dir/shadowsocks.json",
+    notify => Service ["supervisor"]
 } 
 file { "/etc/supervisor/conf.d/shadowsocks.conf":
 	ensure => "present",
 	source => "$dir/shadowsocks.conf",
     require => Package["shadowsocks"],
-    notify => Service ["shadowsocks"]
+    notify => Service ["supervisor"]
 }
 
-service { "supervisor": 
-    ensure => "running",
-    require Package["shadowsocks"]
-}
 exec {"supervisorctl update":
 	command => "/usr/bin/supervisorctl update",
 	logoutput => "true",
     require => File["/etc/supervisor/conf.d/shadowsocks.conf"],
-    notify => Service ["shadowsocks"]
+    notify => Service ["supervisor"]
 }
