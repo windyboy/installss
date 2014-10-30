438d568398d537e51d27578620d9c6780d6c4bba
windyboy <windyboy@gmail.com>
2014-10-30 16:22:43 +0800

    supervisord conf

--- shadowsocks.pp
+++ shadowsocks.pp
@@ -1,5 +1,8 @@
 $dir="home"
-
+$supervisor = $operatingsystem ? {
+    centos => "supervisord",
+    default => "supervisor"
+}
 package {"python-pip": ensure => "installed" }
 package {"python-m2crypto": ensure => "installed" }
 package {"supervisor": ensure => "installed" }
@@ -12,14 +15,20 @@ file {"/etc/supervisor/conf.d":
     ensure => "directory",
     require => Package["supervisor"]
 }
-service {"supervisor":
+service {$supervisor:
     ensure => "running",
     require => Package["supervisor"]
 }
 file { "/etc/shadowsocks.json":
 	ensure => "present",
 	source => "$dir/shadowsocks.json",
-} 
+}
+if $operatingsystem == "centos" {
+   file {"/etc/supervisord.conf":
+        ensure => "present"
+        source => "$dir/supervisord.conf"
+   } 
+}
 file { "/etc/supervisor/conf.d/shadowsocks.conf":
 	ensure => "present",
 	source => "$dir/shadowsocks.conf",
