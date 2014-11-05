8f5b22c3fd91518323a415469575d7b6d648e36c
windyboy <windyboy@gmail.com>
2014-11-05 11:25:58 +0800

    add allow_virtual parameter

--- shadowsocks.pp
+++ shadowsocks.pp
@@ -1,3 +1,11 @@
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
 include supervisord
 $dir="/tmp/sss"
 if $operatingsystem == "centos" {
